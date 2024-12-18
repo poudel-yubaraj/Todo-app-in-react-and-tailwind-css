e408d92d7aba6f8dd01b345c16b68d1487495a2a
Yubaraj Poudel <poudel.yubaraj42@gmail.com>
2024-12-18 14:03:59 +0545

    Todo App completed

--- src/Component/Todo.jsx
+++ src/Component/Todo.jsx
@@ -1,9 +1,51 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import todo_icon from '../assets/todo_icon.png';
 import TodoItem from './TodoItem';
 const Todo = () => {
+    // const [todoList, setTodoList] = useState([])
+    //yaha todos euta key ho
+    //json.parse le string lai arrray ma convert garxa
+    const[todoList,setTodoList]=useState(localStorage.getItem("todos")?JSON.parse(localStorage.getItem("todos")):[]);
+    const inputRef = useRef();
+    //adding list item
+    const add = () => {
+        const inputText = inputRef.current.value.trim();
+        console.log(inputText);
+        if (inputText === "") {
+            return null;
+        }
+        const newTodo = {
+            id: Date.now(),
+            text: inputText,
+            isComplete: false,
+        }
+        setTodoList((prev) => [...prev, newTodo]);
+        inputRef.current.value = "";
+    }
 
-    
+//deleting the list item
+
+const deleteTodo = (id)=>{
+ setTodoList((preTodos)=>{
+    return preTodos.filter((todo)=>todo.id!=id)
+ })
+}
+//for toggling the list items
+const toggle=(id)=>{
+    setTodoList((preTodos)=>{
+        return preTodos.map((todos)=>{
+            if(todos.id===id){
+                return{...todos,isComplete:!todos.isComplete}
+            }
+            return todos
+        })
+    })
+}
+
+useEffect(()=>{
+    console.log(todoList)
+    localStorage.setItem("todos",JSON.stringify(todoList));//it convert the string
+},[todoList])
 
     return (
         <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl' >
@@ -15,16 +57,18 @@ const Todo = () => {
             </div>
             {/* -----------input-box------ */}
 
-        <div className='flex items-center my-y bg-gray-200 rounded-full'>
-            <input  className='border-0 bg-transparent outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task here' />
-            <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD + </button>
-        </div>
+            <div className='flex items-center my-y bg-gray-200 rounded-full'>
+                <input ref={inputRef} className='border-0 bg-transparent outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task here' />
+                <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD + </button>
+            </div>
 
-  {/* -----------todo-list------ */}
-  <div>
-        <TodoItem text="Learn React"/>
-        <TodoItem text="Learn MERN"/>
-  </div>
+            {/* -----------todo-list------ */}
+            <div>
+                {todoList.map((item, index) => {
+                    return <TodoItem key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle} />
+                })}
+
+            </div>
 
         </div>
     )
--- src/Component/TodoItem.jsx
+++ src/Component/TodoItem.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import tick from '../assets/tick.png'
 import not_tick from '../assets/not_tick.png'
 import delete_icon from '../assets/delete.png'
-const TodoItem = ({text}) => {
+const TodoItem = ({text, id ,isComplete,deleteTodo,toggle}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
-        <div className='flex flex-1 items-center cursor-pointer '>
-            <img src={tick} alt="tick icon" className='w-7' />
-            <p className='text-slate-700 ml-7 text-[17px]'>{text} </p>
+        <div onClick={()=>{toggle(id)}} className='flex flex-1 items-center cursor-pointer '>
+            <img src={isComplete?tick:not_tick} alt="tick icon" className='w-7' />
+            <p className={`text-slate-700 ml-7 text-[17px] ${isComplete?"line-through":""} text-decoration-slate-500`}>{text} </p>
         </div>
-        <img src={delete_icon} alt="" className='w-3.5 cursor-pointer' />
+        <img  onClick={()=>{deleteTodo(id)}} src={delete_icon} alt="" className='w-3.5 cursor-pointer' />
     </div>
   )
 }
